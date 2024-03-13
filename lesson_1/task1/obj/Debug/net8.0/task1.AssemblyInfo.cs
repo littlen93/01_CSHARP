0af7bf7bb5c119053ad7184765275a5386ea34ba
Anastasiya Savel'eva <littlen93@mail.ru>
2024-03-13 19:12:44 +0500

    закомитила файлы

--- lesson_1/task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
+++ lesson_1/task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7b792ca2fe968c3dff0b4fe2c1fa88723425924")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+580b9d4dc8bf41fe89456213b1f42861fe49721c")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- lesson_1/task2/Program.cs
+++ lesson_1/task2/Program.cs
@@ -1,8 +1,11 @@
-﻿int i1 = 5;
+﻿# Прибавление одинаковых типов данных
+
+int i1 = 5;
 int i2 = 6;
 int sum = i1 + i2;
 Console.WriteLine(sum);
 
+# Прибавление с разным типом данных 
 long l1 = 6;
 long sum2 =l1 + i1;
 Console.WriteLine(sum2);
@@ -11,12 +14,16 @@ double d1 = 5.5;
 double sum3 = d1 + i1;
 Console.WriteLine(sum3);
 
+# Умножение 
+
 int product1 = i1 * i2;
 long product2 = l1 * i1;
 double product3 = i1 * d1;
 Console.WriteLine(product1);
 Console.WriteLine(product2);
 Console.WriteLine(product3);
+
+# Деление 
 
 int quotient = i1 / i2;
 Console.WriteLine(quotient);
