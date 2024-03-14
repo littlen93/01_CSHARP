ce006eceec675c4492a10e5bd0c2bea8dc9fec01
Anastasiya Savel'eva <littlen93@mail.ru>
2024-03-14 14:10:56 +0500

    добавила первый тест

---
+++ lesson_2/DZ/test1/Program.cs
@@ -0,0 +1,38 @@
+﻿int firstNumber = 5;
+int secondNumber = 6;
+      
+if (firstNumber  > secondNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+else 
+{
+    Console.WriteLine("второе число больше");
+}
+;
+      
+int firstNumber1 = 6;
+int secondNumber1 = 3;
+      
+if (firstNumber1 > secondNumber1)
+{
+    Console.WriteLine("первое число больше");
+}
+else 
+{
+    Console.WriteLine(secondNumber1);
+}
+;
+
+int firstNumber2 = 3;
+int secondNumber2 = 3;
+      
+if (firstNumber2 == secondNumber2)
+{
+    Console.WriteLine("числа равны");
+}
+else 
+{
+    Console.WriteLine("числа не равны");
+}
+;
