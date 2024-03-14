a45d27535b6e2958098d9cc47f6c6b875d996cef
Anastasiya Savel'eva <littlen93@mail.ru>
2024-03-14 18:17:47 +0500

    добавила тест4

--- lesson_2/DZ/test4/Program.cs
+++ lesson_2/DZ/test4/Program.cs
@@ -2,13 +2,16 @@
 //а на выходе выводит все чётные числа от 1 до number (включительно), 
 //после каждого числа должен быть знак пробела.
 
-// Console.WriteLine("Введите число");
 
 int number = Convert.ToInt32(Console.ReadLine());
 int startNumber = 1;
 int number = 5;
 
-while (startNumber<=number)
+while (startNumber <= number)
+{
+    if (startNumber % 2 == 0)
+    {
+        Console.Write(startNumber + " ");
+    }
     startNumber++;
-    if (startNumber % 2 == 1)
-        Console.ReadLine(startNumber);
+}
