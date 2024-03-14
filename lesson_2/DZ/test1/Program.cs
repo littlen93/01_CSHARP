b304ba7e6213da5a0cdfa04f590563f377b96a67
Anastasiya Savel'eva <littlen93@mail.ru>
2024-03-14 17:47:15 +0500

    добавила 3 тест

--- lesson_2/DZ/test1/Program.cs
+++ lesson_2/DZ/test1/Program.cs
@@ -1,38 +1,16 @@
-﻿int firstNumber = 5;
+﻿
+int firstNumber = 5;
 int secondNumber = 6;
-      
-if (firstNumber  > secondNumber)
-{
-    Console.WriteLine(firstNumber);
-}
-else 
+
+if (firstNumber  <  secondNumber)
 {
     Console.WriteLine("второе число больше");
 }
-;
-      
-int firstNumber1 = 6;
-int secondNumber1 = 3;
-      
-if (firstNumber1 > secondNumber1)
+ if (firstNumber  >  secondNumber)
 {
     Console.WriteLine("первое число больше");
 }
-else 
-{
-    Console.WriteLine(secondNumber1);
-}
-;
-
-int firstNumber2 = 3;
-int secondNumber2 = 3;
-      
-if (firstNumber2 == secondNumber2)
+if (firstNumber == secondNumber)
 {
     Console.WriteLine("числа равны");
 }
-else 
-{
-    Console.WriteLine("числа не равны");
-}
-;
---
+++ lesson_2/DZ/test4/Program.cs
@@ -0,0 +1,14 @@
+﻿//напишите метод которая на вход принимает число (number), 
+//а на выходе выводит все чётные числа от 1 до number (включительно), 
+//после каждого числа должен быть знак пробела.
+
+// Console.WriteLine("Введите число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int startNumber = 1;
+int number = 5;
+
+while (startNumber<=number)
+    startNumber++;
+    if (startNumber % 2 == 1)
+        Console.ReadLine(startNumber);
