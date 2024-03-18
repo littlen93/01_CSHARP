e3bea302b89df642bd0ed4fc2442fcca0fe19b06
Anastasiya Savel'eva <littlen93@mail.ru>
2024-03-18 14:46:06 +0500

    добавила DZ

---
+++ lesson_3/DZ/task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая на вход
+// принимает натуральное число N, а на выходе
+// показывает его цифры через запятую.
+
+Console.WriteLine("Ведите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num>9 && num<100)
+{
+
+int num1 = num/10;
+int num2 = num%10;
+
+
+Console.Write(num1+","+num2);
+}
+
+
+
