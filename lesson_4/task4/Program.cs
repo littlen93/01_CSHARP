d765868a3b873a751f9fe8cbe9aff895a04d2354
Anastasiya Savel'eva <littlen93@mail.ru>
2024-03-19 19:27:48 +0500

    добавили задачи

---
+++ lesson_4/task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Дано N гирь с разным весом в случайном
+// порядке. Определить вес самой тяжелой
+// гири.
+
+int n = 10;
+int[] array = { 1, 2, 5, 4, 10, 7, 8, 3, 9, 6 };
+int i = 0;
+int max = array[0];
+
+
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
