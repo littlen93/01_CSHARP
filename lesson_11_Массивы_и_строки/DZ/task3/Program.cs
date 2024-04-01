d0c45384d7eca3204b24a608614020fd30db478a
Anastasiya Savel'eva <littlen93@mail.ru>
2024-04-01 14:31:08 +0500

    добавила урок11

---
+++ lesson_11_Массивы_и_строки/DZ/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните,
+// является ли она палиндромом.
+
+System.Console.WriteLine("Введите строку");
+string text = Console.ReadLine();
+string reverse = string.Join("", text.Reverse());
+
+if(text == reverse)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
---
+++ lesson_11_Массивы_и_строки/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте массив символов (тип char []). Создайте строку из
+// символов этого массива. 
+
+char[] array = new char[] {'l','7','u','9','1','0'};
+
+string str = "";
+
+for (int i = 0; i < array.Length; i++)
+{
+    str = str+ array[i];
+}
+Console.WriteLine(str);
